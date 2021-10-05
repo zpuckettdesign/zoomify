721df32e12bb440f118c860453f2a7726aac853f
zpuckettdesign <zpuckettdesign@gmail.com>
2021-10-05 14:18:37 -0500

    working

---
+++ public/pages/room/src/app.js
@@ -0,0 +1,35 @@
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get("room");
+  console.log("this is the room", room);
+
+  const socketUrl = "http://localhost:3000";
+  // const socketUrl = "https://fast-waters-98059.herokuapp.com";
+  const socketBuilder = new SocketBuilder({ socketUrl });
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      // host: "dry-fjord-14156.herokuapp.com",
+      // secure: true,
+      port: 9000,
+      host: "localhost",
+      path: "/",
+    },
+  });
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
+  const view = new View();
+  const media = new Media();
+  const deps = {
+    view,
+    media,
+    room,
+    socketBuilder,
+    peerBuilder,
+  };
+
+  Business.initialize(deps);
+};
+
+window.onload = onload;
